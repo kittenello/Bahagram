ed4509b20aab9eed7a9d6e022b64d84197e6fe0d
kittenello <marquisyuki10@gmail.com>
2026-09-20 11:22:38 +0300

    Add spy mode persistence settings

--- Bahogram/BGSimpleSettings/Sources/BGSimpleSettings.swift
+++ Bahogram/BGSimpleSettings/Sources/BGSimpleSettings.swift
@@ -6,6 +6,10 @@ public final class BGSimpleSettings {
     private enum Key {
         static let experimentalFeatures = "bahogram.experimentalFeatures"
         static let showFeatureDescriptions = "bahogram.showFeatureDescriptions"
+
+        static let saveDeletedMessages = "bahogram.spy.saveDeletedMessages"
+        static let saveEditHistory = "bahogram.spy.saveEditHistory"
+        static let saveInBotChats = "bahogram.spy.saveInBotChats"
     }
 
     private let defaults: UserDefaults
@@ -15,6 +19,15 @@ public final class BGSimpleSettings {
 
         if defaults.object(forKey: Key.showFeatureDescriptions) == nil {
             defaults.set(true, forKey: Key.showFeatureDescriptions)
+        }
+        if defaults.object(forKey: Key.saveDeletedMessages) == nil {
+            defaults.set(true, forKey: Key.saveDeletedMessages)
+        }
+        if defaults.object(forKey: Key.saveEditHistory) == nil {
+            defaults.set(true, forKey: Key.saveEditHistory)
+        }
+        if defaults.object(forKey: Key.saveInBotChats) == nil {
+            defaults.set(false, forKey: Key.saveInBotChats)
         }
     }
 
@@ -33,6 +46,33 @@ public final class BGSimpleSettings {
         }
         set {
             self.defaults.set(newValue, forKey: Key.showFeatureDescriptions)
+        }
+    }
+
+    public var saveDeletedMessages: Bool {
+        get {
+            return self.defaults.bool(forKey: Key.saveDeletedMessages)
+        }
+        set {
+            self.defaults.set(newValue, forKey: Key.saveDeletedMessages)
+        }
+    }
+
+    public var saveEditHistory: Bool {
+        get {
+            return self.defaults.bool(forKey: Key.saveEditHistory)
+        }
+        set {
+            self.defaults.set(newValue, forKey: Key.saveEditHistory)
+        }
+    }
+
+    public var saveInBotChats: Bool {
+        get {
+            return self.defaults.bool(forKey: Key.saveInBotChats)
+        }
+        set {
+            self.defaults.set(newValue, forKey: Key.saveInBotChats)
         }
     }
 }
